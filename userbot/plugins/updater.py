6c29f295d367171ae14d8667c2a63b7f6bb57fa7
Teameviral <dxpubg61@gmail.com>
2022-01-08 20:03:22 +0000

    auto fixes

--- userbot/plugins/updater.py
+++ userbot/plugins/updater.py
@@ -1,38 +1,26 @@
 import asyncio
-import os
 import sys  # mrunal
+from os import environ, execle, path, remove
 
-import heroku3
-import requests
-import urllib3
 from git import Repo
 from git.exc import GitCommandError, InvalidGitRepositoryError, NoSuchPathError
 
 from userbot import *
-from userbot.Config import Config
 
 from . import *
 
-
-from os import remove, execle, path, makedirs, getenv, environ
-from shutil import rmtree
-import asyncio
-import sys
-
-from git import Repo
-from git.exc import GitCommandError, InvalidGitRepositoryError, NoSuchPathError
-
-
 requirements_path = path.join(
-    path.dirname(path.dirname(path.dirname(__file__))), 'requirements.txt')
+    path.dirname(path.dirname(path.dirname(__file__))), "requirements.txt"
+)
 
 HEROKU_API_KEY = config.HEROKU_API_KEY
 HEROKU_APP_NAME = config.HEROKU_APP_NAME
 GIT_REPO_NAME = "Teameviral"
 UPSTREAM_REPO_URL = "https://github.com/Teameviral/FIREXUSERBOT"
 
+
 async def gen_chlog(repo, diff):
-    ch_log = ''
+    ch_log = ""
     d_form = "On " + "%d/%m/%y" + " at " + "%H:%M:%S"
     for c in repo.iter_commits(diff):
         ch_log += f"**#{c.count()}** : {c.committed_datetime.strftime(d_form)} : [{c.summary}]({UPSTREAM_REPO_URL.rstrip('/')}/commit/{c}) by **{c.author}**\n"
@@ -43,9 +31,10 @@ async def updateme_requirements():
     reqs = str(requirements_path)
     try:
         process = await asyncio.create_subprocess_shell(
-            ' '.join([sys.executable, "-m", "pip", "install", "-r", reqs]),
+            " ".join([sys.executable, "-m", "pip", "install", "-r", reqs]),
             stdout=asyncio.subprocess.PIPE,
-            stderr=asyncio.subprocess.PIPE)
+            stderr=asyncio.subprocess.PIPE,
+        )
         await process.communicate()
         return process.returncode
     except Exception as e:
@@ -66,11 +55,11 @@ async def upstream(ups):
         txt += "some problems occured`\n\n**LOGTRACE:**\n"
         repo = Repo()
     except NoSuchPathError as error:
-        await ups.edit(f'{txt}\n`directory {error} is not found`')
+        await ups.edit(f"{txt}\n`directory {error} is not found`")
         repo.__del__()
         return
     except GitCommandError as error:
-        await ups.edit(f'{txt}\n`Early failure! {error}`')
+        await ups.edit(f"{txt}\n`Early failure! {error}`")
         repo.__del__()
         return
     except InvalidGitRepositoryError as error:
@@ -82,41 +71,47 @@ async def upstream(ups):
             )
             return
         repo = Repo.init()
-        origin = repo.create_remote('upstream', off_repo)
+        origin = repo.create_remote("upstream", off_repo)
         origin.fetch()
         force_updateme = True
-        repo.create_head('main', origin.refs.master)
+        repo.create_head("main", origin.refs.master)
         repo.heads.master.set_tracking_branch(origin.refs.master)
         repo.heads.master.checkout(True)
 
     ac_br = repo.active_branch.name
-    if ac_br != 'main':
+    if ac_br != "main":
         await ups.edit(
-            f'**[UPDATER]:**` Looks like you are using your own custom branch ({ac_br}). '
-            'in that case, Updater is unable to identify '
-             'which branch is to be merged. '
-            'Please checkout the official branch`')
+            f"**[UPDATER]:**` Looks like you are using your own custom branch ({ac_br}). "
+            "in that case, Updater is unable to identify "
+            "which branch is to be merged. "
+            "Please checkout the official branch`"
+        )
         repo.__del__()
         return
 
     try:
-        repo.create_remote('upstream', off_repo)
+        repo.create_remote("upstream", off_repo)
     except BaseException:
         pass
 
-    ups_rem = repo.remote('upstream')
+    ups_rem = repo.remote("upstream")
     ups_rem.fetch(ac_br)
 
-    changelog = await gen_chlog(repo, f'HEAD..upstream/{ac_br}')
+    changelog = await gen_chlog(repo, f"HEAD..upstream/{ac_br}")
 
     if not changelog and not force_updateme:
         await ups.edit(
-            f'\nBot is  **up-to-date**  `with`  **[[{ac_br}]]({UPSTREAM_REPO_URL}/tree/{ac_br})**\n')
+            f"\nBot is  **up-to-date**  `with`  **[[{ac_br}]]({UPSTREAM_REPO_URL}/tree/{ac_br})**\n"
+        )
         repo.__del__()
         return
 
     if conf != "now" and not force_updateme:
-        changelog_str = f'**New UPDATE available for [[{ac_br}]]({UPSTREAM_REPO_URL}/tree/{ac_br}):**\n\n' + '**CHANGELOG**\n\n' + f'{changelog}'
+        changelog_str = (
+            f"**New UPDATE available for [[{ac_br}]]({UPSTREAM_REPO_URL}/tree/{ac_br}):**\n\n"
+            + "**CHANGELOG**\n\n"
+            + f"{changelog}"
+        )
         if len(changelog_str) > 4096:
             await ups.edit("`Changelog is too big, view the file to see it.`")
             file = open("output.txt", "w+")
@@ -130,22 +125,26 @@ async def upstream(ups):
             remove("output.txt")
         else:
             await ups.edit(changelog_str)
-        await ups.respond(f'Do `.update now` to update')
+        await ups.respond(f"Do `.update now` to update")
         return
 
     if force_updateme:
-        await ups.edit(
-            '`Force-Updating to latest stable code, please wait sur😅😅...`')
+        await ups.edit("`Force-Updating to latest stable code, please wait sur😅😅...`")
     else:
-        await ups.edit('`Updating your` **ßoott** `please wait for 5 mins then type .alive/.ping/.help/.test to see if I am On...`')
+        await ups.edit(
+            "`Updating your` **ßoott** `please wait for 5 mins then type .alive/.ping/.help/.test to see if I am On...`"
+        )
     # We're in a Heroku Dyno, handle it's memez.
     if config.HEROKU_API_KEY is not None:
         import heroku3
+
         heroku = heroku3.from_key(config.HEROKU_API_KEY)
         heroku_app = None
         heroku_applications = heroku.apps()
         if not config.HEROKU_APP_NAME:
-            await ups.edit('`Please set up the HEROKU_APP_NAME configiable to be able to update FiReX`')
+            await ups.edit(
+                "`Please set up the HEROKU_APP_NAME configiable to be able to update FiReX`"
+            )
             repo.__del__()
             return
         for app in heroku_applications:
@@ -153,17 +152,17 @@ async def upstream(ups):
                 heroku_app = app
                 break
         if heroku_app is None:
-            await ups.edit(
-                f'{txt}\n`Invalid Heroku credentials for updating.`'
-            )
+            await ups.edit(f"{txt}\n`Invalid Heroku credentials for updating.`")
             repo.__del__()
             return
-        await ups.edit('`Updating Started 😎😎✨\nRestarting, please wait 5min then type .alive to check if I alive!!!🙂`'
-                       )
+        await ups.edit(
+            "`Updating Started 😎😎✨\nRestarting, please wait 5min then type .alive to check if I alive!!!🙂`"
+        )
         ups_rem.fetch(ac_br)
         repo.git.reset("--hard", "FETCH_HEAD")
         heroku_git_url = heroku_app.git_url.replace(
-            "https://", "https://api:" + config.HEROKU_API_KEY + "@")
+            "https://", "https://api:" + config.HEROKU_API_KEY + "@"
+        )
         if "heroku" in repo.remotes:
             remote = repo.remote("heroku")
             remote.set_url(heroku_git_url)
@@ -172,11 +171,13 @@ async def upstream(ups):
         try:
             remote.push(refspec="HEAD:refs/heads/master", force=True)
         except GitCommandError as error:
-            await ups.edit(f'{txt}\n`Here is the error log:\n{error}`')
+            await ups.edit(f"{txt}\n`Here is the error log:\n{error}`")
             repo.__del__()
             return
-        await ups.edit('`Sync Verified Successfully 🙂🙂\n'
-                       'Restarting, please wait a min ,then type .alive to check if I alive 😂!!`')
+        await ups.edit(
+            "`Sync Verified Successfully 🙂🙂\n"
+            "Restarting, please wait a min ,then type .alive to check if I alive 😂!!`"
+        )
     else:
         # Classic Updater, pretty straightforward.
         try:
@@ -184,8 +185,9 @@ async def upstream(ups):
         except GitCommandError:
             repo.git.reset("--hard", "FETCH_HEAD")
         await updateme_requirements()
-        await ups.edit('`Successfully Updated!\n'
-                       'Bot is restarting... Wait for a minute😎😎!`')
+        await ups.edit(
+            "`Successfully Updated!\n" "Bot is restarting... Wait for a minute😎😎!`"
+        )
         # Spin a new instance of bot
         args = [sys.executable, "-m", "userbot"]
         execle(sys.executable, *args, environ)
